98b1a33ee8a06545e4dbd761b339098ceeefa333
daboland <damianvboland@gmail.com>
2017-12-18 01:15:42 +0000

    instructions working

--- app/Code.hs
+++ app/Code.hs
@@ -1,4 +1,4 @@
-module Code(Instruction,Opcode,Field,bldProgram,mkMem)where
+module Code(Instruction,Opcode,Field,bldProgram,mkMem, instruct)where
 
 type Instruction = (Opcode,Field,Field)
     
@@ -15,7 +15,67 @@ data Opcode = MOV
             | SPL
               deriving(Show)
 
+update :: Instruction -> Int -> [Instruction] -> [Instruction]
+update inst adr mem = do 
+                    let (myH, myT) = splitAt adr mem
+                    let myTail = tail myT
+                    let newTail = mappend [inst] myTail
+                    (mappend myH newTail)
 
+instruct :: Instruction-> [Instruction] -> Int -> Bool -> ([Instruction], Int, Bool)
+instruct (DAT, f1, f2) mem pc alive = (mem, (pc+1), False)
+instruct (MOV, f1, f2) mem pc alive = ((update newInst dest mem), (mod (pc+1) (length mem)), alive)
+        where newInst = getInstruct f1 mem pc
+              dest =pc + (getAval f2 mem)
+              
+instruct (ADD,f1,f2) mem pc alive = do 
+                            let x = (pc +(getAval f2 mem))
+                            let inst1 = getInstruct f1 mem pc
+                            let (op,fld1, fld2) = getInstruct f2 mem pc
+                            let (ctrl, no) = fld2
+                            let newfld = (ctrl, (mod ((chkAdd inst1) + no) (length mem)))
+                            ((update (op,fld1,newfld) x mem),(mod (pc+1) (length mem)), alive)
+
+instruct (SUB,f1,f2) mem pc alive= do
+                            let x = (pc +(getAval f2 mem))
+                            let inst1 = getInstruct f1 mem pc
+                            let (op,fld1, fld2) = getInstruct f2 mem pc
+                            let (ctrl, no) = fld2
+                            let newfld = (ctrl, (mod (no - (chkAdd inst1)) (length mem)))
+                            ((update (op,fld1,newfld) x mem),(mod (pc+1) (length mem)), alive)
+instruct (JMP, f1, f2) mem pc alive = do
+                            let newpc = pc +(getAval f1 mem)
+                            (mem, (mod (newpc+1) (length mem)), alive)
+instruct (JMZ, f1, f2) mem pc alive
+                            |(getAval f2 mem) == 0 = instruct (JMP, f1, f2) mem pc alive
+                            |otherwise = (mem,(mod (pc+1) (length mem)), alive)
+instruct (JMN, f1, f2) mem pc alive
+                            |(getAval f2 mem) < 0 = instruct (JMP, f1,f2) mem pc alive
+                            |otherwise = (mem,(mod (pc+1) (length mem)), alive)
+instruct (DJN, f1, f2) mem pc alive
+                            |((getAval f2 mem)-1)<0 = instruct(JMP, f1, f2) mem pc alive
+                            |otherwise = (mem,(mod (pc+1) (length mem)), alive)
+instruct(CMP, f1, f2) mem pc alive
+                            |(getAval f1 mem) == (getAval f2 mem) = (mem, (mod (pc+2) (length mem)), alive)
+                            |otherwise = (mem,(mod (pc+1) (length mem)), alive)
+
+                            
+                            
+
+--if trying to ADD, check if you can first, if not return something you can add with
+chkAdd :: Instruction -> Int
+chkAdd (DAT, _, (Immediate, x)) = x
+chkAdd (_,_,_) = 0
+                            
+--returns instruction at an address, unless immediate. returns a DAT instruction
+getInstruct :: Field -> [Instruction] -> Int -> Instruction
+getInstruct (Direct, x) mem pc = mem!!(pc + x)
+getInstruct (Indirect, x) mem pc = do 
+                                  let (op,f1,f2) = mem!!(pc + x)
+                                  let inst = getInstruct f2 mem pc
+                                  inst
+getInstruct (AutoDecrement, x) mem pc = getInstruct (Indirect, (x-1)) mem pc
+getInstruct (Immediate, x) mem pc = (DAT,(Direct,0),(Immediate, x))
 
 operation:: String -> Opcode
 operation "MOV" = MOV
@@ -40,20 +100,38 @@ data Control = Identity
             |Data
             deriving(Show)
 
---control field
-cf:: [Char] -> Field
-cf ('#':x:_) = (Immediate, (read [x]::Integer))
-cf ('@':x:_) = (Indirect,(read ([x])::Integer))
-cf ('<':x:_) = (AutoDecrement, (read [x]::Integer))
-cf (x:_) = (Direct, (read [x]::Integer))
-cf [] = (Data,0)
+--returns mem addr of operand
+getAval :: Field -> [Instruction]-> Int
+getAval (Direct, i) xs = i
+getAval (Indirect, i) xs = getAval (getBField (xs!!i)) xs
+getAval (Immediate, i) xs = i
+getAval (AutoDecrement, i) xs = i-1
 
-type Field = (Control, Integer)
+
+-- getting second field because indirect address should point to a DAT instuction
+getBField :: Instruction -> Field
+getBField (op,f1,f2) = f2
+
+--parcing control field
+cfA:: [Char] -> Field
+cfA ('#':xs) = (Immediate, (read (init xs)::Int))
+cfA ('@':xs) = (Indirect,(read (init xs)::Int))
+cfA ('<':xs) = (AutoDecrement, (read (init xs)::Int))
+cfA (xs) = (Direct, (read (init xs)::Int))
+
+cfB:: [Char] -> Field
+cfB ('#':xs) = (Immediate, (read xs::Int))
+cfB ('@':xs) = (Indirect, (read xs::Int))
+cfB ('<':xs) = (AutoDecrement, (read xs::Int))
+cfB (xs) = (Direct, (read xs::Int))
+
+
+type Field = (Control, Int)
 
 
 
 bldInstruct:: [String] -> Instruction
-bldInstruct (op:fs) = ( (operation op), (cf (fs!!0)), (cf (fs!!1)) )
+bldInstruct (op:fs) = ( (operation op), (cfA (fs!!0)), (cfB (fs!!1)) )
 
 --get rid of comma in A field
 parseA :: String -> String
@@ -63,15 +141,18 @@ parseA s = init s
 bldProgram :: [String] ->[Instruction]
 bldProgram xs = (map bldInstruct (map splitInstr (xs)))
 
-mkMem:: ([Instruction], Integer) -> [Instruction]
+mkMem:: ([Instruction], Int) -> [Instruction]
 mkMem (xs, 0) = xs
 mkMem (xs, len) = mkMem (newXs, newLen)
           where (newXs, newLen) = (addInstruction xs len)
 
 
-addInstruction :: [Instruction] -> Integer -> ([Instruction], Integer)
+addInstruction :: [Instruction] -> Int -> ([Instruction], Int)
 addInstruction xs len = ((mappend xs [placeHolder]), len-1)
 
 placeHolder::Instruction
 placeHolder = (DAT,(Direct,0), (Direct,0))
 
+
+--execute:: [Instruction] -> Int -> ([Instruction] , Int)
+--execute mem pc = 
--- app/Main.hs
+++ app/Main.hs
@@ -2,14 +2,68 @@ module Main where
 
 import Lib
 import Code
+import Control.Concurrent --(forkIO, threadDelay,writeChan,readChan,newChan)
+import Control.Monad
 
 main :: IO ()
 main = do -- the arrow take an IO action and assigns the resulted action to redcode so now redcode is a string
+  putStr "hi"
   redcode <- readFile "/home/damian/haskell/4thYear/redcode/src/input.txt"
   --arrow is not needed here as there is no IO action anymore
   let code = (lines redcode)
+  let startPos = [0, 250, 500,750]
   let program = (bldProgram code)
-  let mem = (mkMem (program,80))
-  (putStr (show(mem!!55)))
-  
+  let mem = (mkMem (program,1000))
+  memChan <- newChan
+  pc1 <-newChan
+  writeChan memChan mem
+  writeChan pc1 (startPos!!0)
+  t1 <- forkIO (forever $ (war True pc1 memChan))
+  manage <- forkIO (forever $ (manager [(pc1,t1)] ))
+  newMem <- readChan memChan
+  printMem newMem 0
 
+manager :: [(Chan Int, ThreadId)] -> IO()
+manager [] = putStr ""
+manager ((chan, tId):xs) = do
+                        chkThread chan tId
+                        manager xs
+
+chkThread :: Chan Int -> ThreadId -> IO ()
+chkThread c1 t1 = do
+            pc <- readChan c1 
+            if (pc == (-1))
+              then killThread t1
+              else writeChan c1 pc
+
+
+printMem :: [Instruction] -> Int -> IO ()
+printMem mem 15 = putStr ""
+printMem mem i = do 
+                (putStrLn (show(mem!!i)))
+                printMem mem (i+1)
+
+--wrapper function to return IO tuple
+myLift :: ([Instruction],Int,Bool) -> IO ([Instruction],Int, Bool)
+myLift tup = return tup
+
+--executes instruction
+war :: Bool -> Chan Int -> Chan [Instruction] -> IO ()
+war alive pc c
+    |alive == False = writeChan pc (-1)
+    |otherwise = do
+                mem <- readChan c
+                crntPc <- readChan pc
+                let (newMem, newPc, live) = instruct (mem!!crntPc) mem crntPc alive
+                writeChan c mem 
+                writeChan pc newPc
+
+startProgram :: [Instruction] -> Int -> Bool -> IO ([Instruction], Int, Bool)
+startProgram mem pc alive
+                      | pc == (length mem) = (myLift (mem, 0, alive))
+                      | otherwise = (startProgram newMem newPc newState)
+                      where (newMem, newPc, newState) = (instruct (mem!!pc) mem pc alive)
+
+--doInstructions :: [Instruction] -> Int -> Bool -> ([Instruction], Int, Bool)
+--doInstructions mem pc alive =(doInstructions newMem newPc alive)
+--              where (newMem, newPc, _) = startProgram mem pc alive
